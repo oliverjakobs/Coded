ed8a4735044bf2e7e74a9132296a88ba04c0d369
Oliver <oliver.jakobs1995@googlemail.com>
2019-07-28 23:52:25 +0200

    pyqt5 titebar

--- style.py
+++ style.py
@@ -11,17 +11,20 @@ class JSONStyle:
     
 if __name__ == "__main__":
     import tkinter as tk
-    from tkinter import ttk
-    from ttkthemes import ThemedTk
 
-    root = ThemedTk(theme="black")
-    root.geometry("600x400")
+    def move_window(event):
+        app.geometry('+{0}+{1}'.format(event.x_root, event.y_root))
 
-    nb = ttk.Notebook()
-    nb.pack()
-
-    print(root.get_themes())
-
-    nb.add(ttk.Label(text="Hello Style"), text="Tab")
-
-    root.mainloop()
+    app = tk.Tk()
+    app.overrideredirect(True)
+    screen_width = app.winfo_screenwidth()
+    screen_height = app.winfo_screenheight()
+    x_coordinate = (screen_width/2) - (1050/2)
+    y_coordinate = (screen_height/2) - (620/2)
+    app.geometry("{}x{}+{}+{}".format(1050, 650, int(x_coordinate), int(y_coordinate)))
+    title_bar = tk.Frame(app, bg='#090909', relief='raised', bd=0, height=20, width=1050)
+    close_button = tk.Button(title_bar, text='X', command=app.destroy, width=5, bg="#090909", fg="#888", bd=0)
+    title_bar.place(x=0, y=0)
+    close_button.place(rely=0, relx=1, x=0, y=0, anchor=tk.NE)
+    title_bar.bind('<B1-Motion>', move_window)
+    app.mainloop()
